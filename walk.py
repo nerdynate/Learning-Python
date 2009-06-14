6f41c25054413734e5a79dde69873cac94ac15b8
nerdynate <irishpunkrock@gmail.com>
2009-06-14 17:47:17 -0400

    updated walk.py to actually work

--- walk.py
+++ walk.py
@@ -4,13 +4,13 @@
 import os,sys,stat
 
 # Path we want to search
-backuppath = '/pool/static/backup/clients/'
+backuppath = '/tmp/'
 
 def find_files( backuppath  ):
     for root, dirs, files, in os.walk(backuppath):
         for name in files:
             filename = os.path.join(root, name)
-            if filename.endswith('.sql'):
+            if filename.endswith('.txt'):
                print filename
 
-find_files
+find_files(backuppath)
