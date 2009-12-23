a35be859cb5248185c8a747c2b1c02d4a6e61714
Nate G <nate.geouge@escapemg.com>
2009-12-23 14:43:48 -0500

    testing

--- walk.py
+++ walk.py
@@ -7,15 +7,13 @@ import os,sys,stat
 path = '/tmp/'
 
 # Store files 
-files=[]
+files = []
 
 def find_files( path  ):
     for root, dirs, files, in os.walk(path):
         for name in files:
             filename = os.path.join(root, name)
-            if filename.endswith('.pdf'):
-               files.append(filename)
+            if filename.endswith('.txt'):
                print filename
 
 find_files(path)
-print files
